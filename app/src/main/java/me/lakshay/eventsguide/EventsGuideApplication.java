553a1617c34f3a789689a8432cc0cdb1651f6414
Lakshay Chhikara <lakshay.chhikara09@gmail.com>
2017-01-24 21:57:49 +0530

    leveraging Firebase's offline capabilities for caching

--- app/src/main/java/me/lakshay/eventsguide/EventsGuideApplication.java
+++ app/src/main/java/me/lakshay/eventsguide/EventsGuideApplication.java
@@ -2,11 +2,13 @@ package me.lakshay.eventsguide;
 
 import android.app.Application;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class EventsGuideApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
-		//FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+		FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 	}
 }
